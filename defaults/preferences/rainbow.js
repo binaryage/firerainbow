ce3ddd35d22ff7f3a3a021309e344e4530d58e6d
Antonin Hildebrand <antonin@hildebrand.cz>
2008-06-13 19:51:20 +0000

    updated default color profile
    git-svn-id: https://rainbow4firebug.googlecode.com/svn/trunk@15 210b2b9c-644f-0410-8a67-11deb4329d02

--- defaults/preferences/rainbow.js
+++ defaults/preferences/rainbow.js
@@ -5,5 +5,4 @@ pref("extensions.rainbow.linesPerCall", 20);
 //pref("extensions.rainbow.coloring", ".panelNode-script {overflow: hidden; font-family: Courier New; font-size: 9pt; } .keyword { color: #770088; } .atom { color: #228811; } .variable { color: black; } .variabledef { color: #0000FF; } .localvariable { color: #004499; } .property { color: black; } .comment { color: #AA7700; } .string { color: #AA2222; }");
 
 // netbeans
-pref("extensions.rainbow.coloring", ".panelNode-script {font-family:Monospaced,Courier New;font-size:12px;font-weight:bold;overflow:hidden;} .keyword {color:blue;} .atom {color:black;} .variable {color:#009900;} .variabledef {color:black;} .localvariable {color:black;} .property {color:black;} .comment {color:#969696;} .string {color:#CE7B00;} .regexp {color:#9933CC;} .punctuation {color:black;} .operator {color:black;} .whitespace {color:black;}");
-
+pref("extensions.rainbow.coloring", ".panelNode-script {font-family:Monospaced,Courier New;font-size:12px;font-weight:bold;overflow:hidden;} .keyword {color:blue;} .atom {color:blue;} .variable {color:black;} .variabledef {color:#005500;} .localvariable {color:#009900;} .property {color:black;} .comment {color:#969696;} .string {color:#CE7B00;} .regexp {color:#9933CC;} .punctuation {color:black;} .operator {color:black;} .whitespace {color:black;}");
